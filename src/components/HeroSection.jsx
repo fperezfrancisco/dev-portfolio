531669f7e7e8d82fde0b8cf1a3074fe61d9e5f96
Francisco Perez <fperezfrancisco4@gmail.com>
2023-11-28 11:25:37 -0800

    added ghpages dependencies

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,11 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section className="sectionContainer flex heroSection" id="hero">
-      <h1 className="mainHeading">Francisco J. Perez</h1>
-      <h2 className="subHeading">Front-End Developer</h2>
-      <button className="pageBtn resumeBtn">Resume</button>
-      <div className="linksSection"></div>
-      <div className="artworkSection">
-        <div className="artBox" id="lowLeft">
-          <div className="artImgHero flex" id="lowLeftImg"></div>
-        </div>
-        <div className="artBox" id="highRight">
-          <div className="artImgHero flex" id="highRightImg"></div>
-        </div>
+      <div className="noisyOverlay"></div>
+      <div className="heroCopy flex">
+        <h1 className="mainHeading">Francisco J. Perez</h1>
+        <h2 className="subHeading">Front-End Developer</h2>
+        <button className="pageBtn resumeBtn">Resume</button>
       </div>
     </section>
   );
